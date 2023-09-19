a737525dc17ff6e0057a6f6bc16e8ee7ac4f9c86
Jack <jack@clicksuite.co.nz>
2023-09-19 21:06:06 +1200

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Jack on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
